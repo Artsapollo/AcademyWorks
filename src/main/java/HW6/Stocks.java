d1c85a5e0839e503ff4fd0c213be0b1f0bd31cd0
Artem Lemeshko <lemart03@gmail.com>
2019-01-01 21:18:33 +0200

    Added Gold class

---
+++ src/main/java/HW6/Stocks.java
@@ -0,0 +1,15 @@
+package HW6;
+
+public class Stocks implements Assets{
+    public void balance() {
+        System.out.println("Show balance");
+    }
+
+    public void operations() {
+        System.out.println("Do some operations");
+    }
+
+    public void transactions() {
+        System.out.println("Make transactions");
+    }
+}

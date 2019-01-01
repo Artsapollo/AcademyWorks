9ac16f7485145872dd38aba5a841442554c68700
Artem Lemeshko <lemart03@gmail.com>
2019-01-01 21:25:48 +0200

    Updated Bank class

--- src/main/java/HW6/Bank.java
+++ src/main/java/HW6/Bank.java
@@ -1,4 +1,26 @@
 package HW6;
 
-public class Bank {
+public class Bank <A extends Assets> {
+    private String name;
+    private String country;
+    private String address;
+    public A assets;
+
+    public Bank(String name, String country, String address) {
+        this.name = name;
+        this.country = country;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }

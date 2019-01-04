b83a32863854e26d8f8a5aa2672113c12cc01ec3
Artem Lemeshko <lemart03@gmail.com>
2019-01-04 18:56:57 +0200

    Made changes

--- src/main/java/CollectionsHW/Fruits.java
+++ src/main/java/CollectionsHW/Fruits.java
@@ -1,50 +1,60 @@
 package CollectionsHW;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-public class Fruits {
-    private Enum type;
-    private int expire;
+public class Fruits implements FruitMood {
+    private FruitsType type;
+    private Integer expire;
     private LocalDate date;
-    private int price;
+    private Integer price;
 
-    public Fruits(Enum type, int expire, LocalDate date, int price) {
+    public Fruits(FruitsType type, int expire, LocalDate date, int price) {
         this.type = type;
         this.expire = expire;
         this.date = date;
         this.price = price;
     }
 
-    public Enum getType() {
+    @Override
+    public FruitsType getType() {
         return type;
     }
 
-    public void setType(Enum type) {
-        this.type = type;
-    }
-
-    public int getExpire() {
+    @Override
+    public Integer getExpirationDays() {
         return expire;
     }
 
-    public void setExpire(int expire) {
-        this.expire = expire;
-    }
-
+    @Override
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public int getPrice() {
+    @Override
+    public Integer getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    @Override
+    public void setPrice(Integer price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fruits)) return false;
+        Fruits fruits = (Fruits) o;
+        return expire == fruits.expire &&
+                price == fruits.price &&
+                getType() == fruits.getType() &&
+                Objects.equals(getDate(), fruits.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getType(), expire, getDate(), price);
     }
 
     @Override
--- src/main/java/CollectionsHW/Store.java
+++ src/main/java/CollectionsHW/Store.java
@@ -4,69 +4,68 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Store {
-    public static List<Fruits> listOfFruits() {
-        List<Fruits> fruitList = new ArrayList<>();
+    private List<Fruits> fruitsList;
 
-        Fruits apple = new Fruits(FruitsType.APPLE, 20, LocalDate.of(2019, 1, 1), 15);
-        Fruits straw = new Fruits(FruitsType.STRAWBERRY, 5, LocalDate.of(2018, 11, 3), 20);
-        Fruits pear = new Fruits(FruitsType.PEAR, 19, LocalDate.of(2017, 10, 4), 40);
-        Fruits pineapple = new Fruits(FruitsType.PINEAPPLE, 3, LocalDate.of(2018, 12, 1), 130);
-        Fruits blueberry = new Fruits(FruitsType.BLUEBERRY, 7, LocalDate.of(2019, 1, 10), 900);
-        Fruits dragon = new Fruits(FruitsType.DRAGONFRUIT, 90, LocalDate.of(2018, 10, 23), 1000);
-        Fruits dragon1 = new Fruits(FruitsType.DRAGONFRUIT, 90, LocalDate.of(2018, 12, 16), 1200);
-        Fruits dragon2 = new Fruits(FruitsType.DRAGONFRUIT, 90, LocalDate.of(2018, 8, 2), 800);
-
-        fruitList.add(apple);
-        fruitList.add(straw);
-        fruitList.add(pear);
-        fruitList.add(pineapple);
-        fruitList.add(blueberry);
-        fruitList.add(dragon);
-        fruitList.add(dragon1);
-        fruitList.add(dragon2);
-
-        return fruitList;
+    public Store(Fruits... fruits) {
+        fruitsList = Arrays.asList(fruits);
     }
 
-    public static void freshFruits(List<Fruits> list) {
-        for (Fruits find : list) {
-            if (find.getDate().isAfter(LocalDate.of(2018, 12, 12))) {
-                System.out.println("The freshest " + find);
+    public void freshFruits(LocalDate date) {
+        List<Fruits> list = new ArrayList<>();
+        for (Fruits find : fruitsList) {
+            if (find.getDate().isAfter(date)) {
+                list.add(find);
             }
         }
+
+        System.out.println("The Freshest " + list.toString());
     }
 
-    public static void fruitsType(List<Fruits> list) {
-        for (Fruits find : list) {
-            if (find.getType().equals(FruitsType.STRAWBERRY) || find.getType().equals(FruitsType.PINEAPPLE)) {
-                System.out.println("Those Dragonfruit still good " + find);
+    public void fruitsType(FruitsType type) {
+        List<Fruits> list = new ArrayList<>();
+        for (Fruits find : fruitsList) {
+            if (find.getType().equals(type)) {
+                list.add(find);
             }
         }
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("Yummy " + list.toString());
     }
 
-    public static void expireTime(List<Fruits> list) {
-        for (Fruits find : list) {
-            if (find.getExpire() > 10) {
-                System.out.println("What about expiration " + find);
+    public List<Fruits> expireTime(Integer expire) {
+        List<Fruits> list = new ArrayList<>();
+        for (Fruits find : fruitsList) {
+            if (find.getExpirationDays() > expire) {
+                list.add(find);
             }
         }
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("What about expiration " + list.toString());
+        return list;
     }
 
-    public static void stillFresh(List<Fruits> list) {
-        for (Fruits find : list) {
-            if (find.getType().equals(FruitsType.DRAGONFRUIT) && find.getDate().isAfter(LocalDate.of(2018, 10, 6))) {
-                System.out.println("Those Dragonfruits still good " + find);
+    public List<Fruits> stillFresh(FruitsType type, LocalDate date) {
+        List<Fruits> list = new ArrayList<>();
+        for (Fruits find : fruitsList) {
+            if (find.getType().equals(type) && find.getDate().isAfter(date)) {
+                list.add(find);
             }
         }
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("Those are still good " + list.toString());
+        return list;
     }
 
-    public static void letsDempThePrice(List<Fruits> list) {
-        for (Fruits find : list) {
-            if (find.getType().equals(FruitsType.STRAWBERRY) && find.getExpire() < 10
-                    || find.getType().equals(FruitsType.PEAR) & find.getExpire() < 24) {
-                find.setPrice(10);
-                System.out.println("Reduced price " + find);
+    public List<Fruits> letsCutThePrice(FruitsType type, Integer expire, Double discount) {
+        List<Fruits> list = new ArrayList<>();
+        for (Fruits find : fruitsList) {
+            if (find.getType().equals(type) && find.getExpirationDays() < expire) {
+                find.setPrice((int) (find.getPrice() * discount));
+                list.add(find);
             }
         }
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("Reducing price " + list.toString());
+        return list;
     }
 }

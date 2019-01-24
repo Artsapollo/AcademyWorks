3464f92c6ecc16b82215d4cb837e989ac1178628
Artem Lemeshko <lemart03@gmail.com>
2019-01-24 15:42:43 +0200

    HW12.2

--- src/test/java/HW11/FruitShopMockSpyTest.java
+++ src/test/java/HW11/FruitShopMockSpyTest.java
@@ -1,8 +1,5 @@
 package HW11;
 
-import static HW11.FruitType.APPLE;
-import static junit.framework.TestCase.assertEquals;
-
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -16,56 +13,79 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FruitShopMockSpyTest {
-    private List<Vitamins> vitamins = Arrays.asList(Vitamins.A);
-    private Fruit apple = new Fruit(APPLE, 10, LocalDate.of(2019, 1, 1), 10, vitamins);
     @Spy
     @InjectMocks
     private FruitShop fruitShop;
     @Mock
     private Fruit appleMock;
+    @Mock
+    private Fruit orangeMock;
+    @Mock
+    private Fruit pearMock;
 
-    @BeforeClass
-    public static void beforeClass() {
-        System.out.println("Before class");
-    }
 
     @Before
     public void before() {
-        System.out.println("Before");
+        when(appleMock.getFruitType()).thenReturn(FruitType.APPLE);
+        when(appleMock.getDayToLive()).thenReturn(30);
+        when(appleMock.getDeliveryDate()).thenReturn(LocalDate.of(2019, 1, 10));
+
+        when(orangeMock.getFruitType()).thenReturn(FruitType.ORANGE);
+        when(orangeMock.getDayToLive()).thenReturn(20);
+        when(orangeMock.getDeliveryDate()).thenReturn(LocalDate.of(2019, 1, 15));
+
+        when(pearMock.getFruitType()).thenReturn(FruitType.PEAR);
+        when(pearMock.getDayToLive()).thenReturn(10);
+        when(pearMock.getDeliveryDate()).thenReturn(LocalDate.of(2019, 1, 20));
+
+        doReturn(Arrays.asList(appleMock, orangeMock, pearMock)).when(fruitShop).getFruits();
     }
 
-    @After
-    public void after() {
-        System.out.println("After");
-    }
 
-    @AfterClass
-    public static void afterClass() {
-        System.out.println("After class");
+    @Test(expected = NullPointerException.class)
+    public void testAllFresh() {
+        List<Fruit> expected = Arrays.asList(appleMock);
+        List<Fruit> actual = fruitShop.allFresh(LocalDate.of(2019, 1, 15));
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void mockTestAllFruitsOfFruitType() {
-        when(appleMock.getFruitType()).thenReturn(FruitType.APPLE);
+    public void testAllFruitsOfFruitType() {
         FruitType expected = FruitType.APPLE;
         FruitType actual = appleMock.getFruitType();
         assertEquals(expected, actual);
     }
 
     @Test
-    public void mockTestDaysToLive() {
-        when(appleMock.getDayToLive()).thenReturn(10);
-        int expected = 10;
-        int actual = appleMock.getDayToLive();
+    public void testByExpireDate() {
+        List<Fruit> expected = Arrays.asList(appleMock);
+        List<Fruit> actual = fruitShop.checkByExpireDate(20);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testVerifyAllFresh() {
+        fruitShop.allFresh(LocalDate.of(2019, 1, 24));
+        verify(fruitShop, times(1)).getFruits();
+        verify(appleMock, times(1)).getDayToLive();
+        verify(orangeMock, times(1)).getDayToLive();
+        verify(pearMock, times(1)).getDayToLive();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAllFreshAndFruitType() {
+        List<Fruit> expected = Arrays.asList(pearMock);
+        List<Fruit> actual = fruitShop.allFreshAndFruitType(FruitType.PEAR, LocalDate.of(2019, 1, 15));
         assertEquals(expected, actual);
     }
 
     @Test
-    public void mockTestDeliveryDate() {
+    public void testDeliveryDate() {
         when(appleMock.getDeliveryDate()).thenReturn(LocalDate.of(2019, 1, 9));
         LocalDate expected = LocalDate.of(2019, 1, 9);
         LocalDate actual = appleMock.getDeliveryDate();
@@ -73,23 +93,31 @@ public class FruitShopMockSpyTest {
     }
 
     @Test
-    public void verifyingAddingToFruitsList() {
+    public void verifyAddingToFruitsList() {
         List fruits = Mockito.spy(new ArrayList());
-        fruits.add(apple);
-        verify(fruits).add(apple);
+        fruits.add(appleMock);
+        verify(fruits).add(appleMock);
         assertEquals(1, fruits.size());
     }
 
     @Test
-    public void stubbingFruitsSpyList() {
+    public void stubAtFruitsSpyList() {
         List spyFruits = Mockito.spy(new ArrayList<>());
-        spyFruits.add(apple);
-        spyFruits.add(apple);
-        spyFruits.add(apple);
+        spyFruits.add(appleMock);
+        spyFruits.add(appleMock);
+        spyFruits.add(appleMock);
 
         assertEquals(3, spyFruits.size());
 
         Mockito.doReturn(50).when(spyFruits).size();
         assertEquals(50, spyFruits.size());
     }
+
+    @Test
+    public void testAllFruitOfFruitTypeVerify() {
+        fruitShop.allFruitOfFruitType(FruitType.PEAR);
+        verify(fruitShop, times(1)).getFruits();
+        verify(pearMock, times(1)).getFruitType();
+    }
+
 }

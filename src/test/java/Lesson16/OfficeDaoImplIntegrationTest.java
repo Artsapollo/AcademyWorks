4f60ddbeb9964e0652f1cf75d29faf96eb152515
Artem Lemeshko <lemart03@gmail.com>
2019-02-14 15:36:19 +0200

    HW19 Changes

--- src/test/java/Lesson16/OfficeDaoImplIntegrationTest.java
+++ src/test/java/Lesson16/OfficeDaoImplIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.sql.SQLException;
 import java.util.Set;
 
-import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 public class OfficeDaoImplIntegrationTest {
     public OfficeDao officeDao = new OfficeDaoImpl();
@@ -14,13 +14,13 @@ public class OfficeDaoImplIntegrationTest {
     public void getAllOffices() throws SQLException{
         Set<Office> offices = officeDao.getAllOffices();
         System.out.println(offices);
-        assertNotNull(offices);
+        assertTrue(offices.size() != 0);
     }
 
     @Test
     public void testFindOfficeByCity() throws SQLException {
         Set<Office> office = officeDao.getOfficeByCity("Бубурино");
         System.out.println(office);
-        assertNotNull(office);
+        assertTrue(office.size() != 0);
     }
 }
